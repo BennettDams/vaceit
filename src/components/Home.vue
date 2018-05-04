afc9106a1411b1e7df4b80f2cac3cc4b9b60a765
BennettDams <dams.bennett@gmail.com>
2018-05-05 00:18:29 +0200

    Add "Home" Vue component

---
+++ src/components/Home.vue
@@ -0,0 +1,44 @@
+<template>
+<div class="home">
+  <div class="holder">
+    <ul>
+      <li v-for="(data, index) in skills" :key="index">{{ data.skill }}</li>
+    </ul>
+
+    <!-- set class to "alert" if "showAlert" is false -->
+    <div v-bind:class="alertObject"></div>
+
+    <p>
+      Theses are the skills:
+    </p>
+
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    // msg: String
+  },
+  data() {
+    return {
+      skills: [{
+          "skill": "Java"
+        },
+        {
+          "skill": "Spring"
+        }
+      ],
+      alertObject: {
+        alert: true
+      },
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style src="./Home.css" scoped>
+</style>
