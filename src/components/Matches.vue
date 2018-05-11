fe7db4f925ec63d31c9d92e8a3fc54cf6cfaea7a
BennettDams <dams.bennett@gmail.com>
2018-05-11 21:55:45 +0200

    Add Matches Vue component

---
+++ src/components/Matches.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="matches">
+
+    <div class="section center-align">
+      <div class="row">
+        <div class="col s12">
+
+          <ul class="collapsible expandable">
+            <li class="active">
+              <div class="collapsible-header transparent"><i class="material-icons">filter_drama</i>First</div>
+              <div class="collapsible-body orange lighten-4 orange-text text-darken-4"><span>Lorem ipsum dolor sit amet.</span></div>
+            </li>
+            <li>
+              <div class="collapsible-header transparent"><i class="material-icons">place</i>Second</div>
+              <div class="collapsible-body transparent"><span>Lorem ipsum dolor sit amet.</span></div>
+            </li>
+            <li>
+              <div class="collapsible-header transparent"><i class="material-icons">whatshot</i>Third</div>
+              <div class="collapsible-body transparent"><span>Lorem ipsum dolor sit amet.</span></div>
+            </li>
+          </ul>
+
+        </div>
+      </div>
+    </div>
+
+  </div>
+  </template>
+
+  <script>
+  export default {
+    name: 'matches',
+    mounted() {
+      this.initializeCollapsible();
+    },
+    methods: {
+      initializeCollapsible() {
+        var elem = document.querySelector('.collapsible');
+        new M.Collapsible(elem, {
+          accordion: false,
+          inDuration: 200,
+          outDuration: 200
+        });
+      }
+    }
+  }
+  </script>
+
+  <style src="./css/Matches.css">
+  </style>
--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -16,54 +16,23 @@
     </div>
   </div>
 
-
-  <div class="section center-align">
-    <div class="row">
-      <div class="col s12">
-
-        <ul class="collapsible expandable">
-          <li class="active">
-            <div class="collapsible-header transparent"><i class="material-icons">filter_drama</i>First</div>
-            <div class="collapsible-body orange lighten-4 orange-text text-darken-4"><span>Lorem ipsum dolor sit amet.</span></div>
-          </li>
-          <li>
-            <div class="collapsible-header transparent"><i class="material-icons">place</i>Second</div>
-            <div class="collapsible-body transparent"><span>Lorem ipsum dolor sit amet.</span></div>
-          </li>
-          <li>
-            <div class="collapsible-header transparent"><i class="material-icons">whatshot</i>Third</div>
-            <div class="collapsible-body transparent"><span>Lorem ipsum dolor sit amet.</span></div>
-          </li>
-        </ul>
-
-      </div>
-    </div>
-  </div>
+  <Matches></Matches>
 
 </div>
 </template>
 
 <script>
+import Matches from './Matches.vue'
 // import axios from 'axios'
 export default {
   name: 'Stats',
+  components: {
+    Matches
+  },
   props: {},
   data() {
     return {
       test: []
-    }
-  },
-  mounted() {
-    this.initializeCollapsible();
-  },
-  methods: {
-    initializeCollapsible() {
-      var elem = document.querySelector('.collapsible');
-      new M.Collapsible(elem, {
-        accordion: false,
-        inDuration: 200,
-        outDuration: 200
-      });
     }
   }
 }
